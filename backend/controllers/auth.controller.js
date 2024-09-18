5ae03c9f2dc3eea7e8fdaa53cd20c87681414cdb
kristoff.riley <kristoff.riley@karibtech.org>
2024-09-17 19:59:07 -0700

    login + logout

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -2,14 +2,45 @@ import { User } from "../models/user.model.js";
 import bcrypt from "bcryptjs";
 import { generateTokenAndSetCookie } from "../utils/generateToken.js";
 
-export const login = (req, res) => {
+export const login = async (req, res) => {
     try {
-    } catch (error) {}
+        const { username, password } = req.body;
+        const user = await User.findOne({ username });
+
+        const isPasswordCorrect = await bcrypt.compare(
+            password,
+            user?.password || ""
+        );
+
+        if (!user || !isPasswordCorrect) {
+            return res
+                .status(400)
+                .json({ error: "Invalid username or password" });
+        }
+
+        generateTokenAndSetCookie(user._id, res);
+
+        res.status(200).json({
+            _id: user._id,
+            fullName: user.fullName,
+            username: user.username,
+            profilePic: user.profilePic,
+        });
+    } catch (error) {
+        console.log("Error in login controller", error);
+        res.status(500).json({ error: "Internal server error" });
+    }
 };
 
-export const logout = (req, res) => {
+export const logout = async (req, res) => {
     try {
-    } catch (error) {}
+        res.cookie("jwt", "", { maxAge: 0 });
+
+        res.status(200).json({ message: "Logged out successfully" });
+    } catch (error) {
+        console.log("Error in login controller", error);
+        res.status(500).json({ error: "Internal server error" });
+    }
 };
 
 export const signup = async (req, res) => {
